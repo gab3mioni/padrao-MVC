19b8744bc2d3f6f5d726fe14c336bcee04ff016e
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-10-06 19:47:53 -0300

    Adicionando CRUD
    Adicionando métodos CRUD

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -14,10 +14,37 @@ class UserController extends Controller
         $this->view('user', ['users' => $users]);
     }
 
-    public function create($name)
+    public function create()
     {
-        $userModel = new UserModel();
-        $userModel->createUser($name);
-        header('Location: /user');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = $_POST['name'];
+            $userModel = new UserModel();
+            $userModel->createUser($name);
+            header('Location: /user');
+            exit();
+        }
+    }
+
+    public function update()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $userModel = new UserModel();
+            $userModel->updateUser($id, $name);
+            header('Location: /user');
+            exit();
+        }
+    }
+
+    public function delete()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $userModel = new UserModel();
+            $userModel->deleteUser($id);
+            header('Location: /user');
+            exit();
+        }
     }
 }
--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -19,4 +19,26 @@ class UserModel
         $stmt = $this->pdo->query("SELECT * FROM users");
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
+
+    public function createUser($name)
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO users (name) VALUES (:name)");
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+    }
+
+    public function updateUser($id, $name)
+    {
+        $stmt = $this->pdo->prepare("UPDATE users SET name = :name WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+    }
+
+    public function deleteUser($id)
+    {
+        $stmt = $this->pdo->prepare("DELETE FROM users WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
 }
