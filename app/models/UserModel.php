51e86596d41f7108b501aadaa8818561d6e69fdb
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-11-07 13:10:57 -0300

    refactor: Adicionando tipo primitivo para as variáveis e tipo de retorno

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -20,25 +20,33 @@ class UserModel
         return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    public function createUser($name): void
+    public function findUserByName(string $name): mixed
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE name = :name");
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function createUser(string $name): bool
     {
         $stmt = $this->pdo->prepare("INSERT INTO users (name) VALUES (:name)");
         $stmt->bindParam(':name', $name);
-        $stmt->execute();
+        return $stmt->execute();
     }
 
-    public function updateUser($id, $name): void
+    public function updateUser(int $id, string $name): bool
     {
         $stmt = $this->pdo->prepare("UPDATE users SET name = :name WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->bindParam(':name', $name);
-        $stmt->execute();
+        return $stmt->execute();
     }
 
-    public function deleteUser($id): void
+    public function deleteUser(int $id): bool
     {
         $stmt = $this->pdo->prepare("DELETE FROM users WHERE id = :id");
         $stmt->bindParam(':id', $id);
-        $stmt->execute();
+        return $stmt->execute();
     }
 }
