43d35f5ac72e17deea0bbb851eb90a3dd3a79515
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-11-07 15:21:55 -0300

    refactor: Adequando UserModel para BaseModel

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -4,49 +4,38 @@ namespace App\Models;
 
 use PDO;
 
-class UserModel
+class UserModel extends BaseModel
 {
-    private $pdo;
-
-    public function __construct()
-    {
-        global $pdo;
-        $this->pdo = $pdo;
-    }
-
     public function getAllUsers(): array
     {
-        $stmt = $this->pdo->query("SELECT * FROM users");
-        return $stmt->fetchAll(PDO::FETCH_OBJ);
+        $query = "SELECT * FROM users";
+        return $this->fetchAllValues($query);
     }
 
-    public function findUserByName(string $name): mixed
+    public function findUserByName(string $name): bool
     {
-        $stmt = $this->pdo->prepare("SELECT * FROM users WHERE name = :name");
-        $stmt->bindParam(':name', $name);
-        $stmt->execute();
-        return $stmt->fetch(PDO::FETCH_OBJ);
+        $query = "SELECT * FROM users WHERE name = :name";
+        return $this->fetchSingleValue($query, [':name' => $name]);
     }
 
     public function createUser(string $name): bool
     {
-        $stmt = $this->pdo->prepare("INSERT INTO users (name) VALUES (:name)");
-        $stmt->bindParam(':name', $name);
-        return $stmt->execute();
+        $query = "INSERT INTO users (name) VALUES (:name)";
+        return $this->executeQuery($query, [':name' => $name]);
     }
 
     public function updateUser(int $id, string $name): bool
     {
-        $stmt = $this->pdo->prepare("UPDATE users SET name = :name WHERE id = :id");
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':name', $name);
-        return $stmt->execute();
+        $query = "UPDATE users SET name = :name WHERE id = :id";
+        return $this->executeQuery($query, [
+            'id' => $id,
+            ':name' => $name
+        ]);
     }
 
     public function deleteUser(int $id): bool
     {
-        $stmt = $this->pdo->prepare("DELETE FROM users WHERE id = :id");
-        $stmt->bindParam(':id', $id);
-        return $stmt->execute();
+        $query = "DELETE FROM users WHERE id = :id";
+        return $this->executeQuery($query, ['id' => $id]);
     }
 }
